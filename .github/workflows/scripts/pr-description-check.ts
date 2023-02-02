ff921269e5fdde63e39f549c7a90c7ee09499d3a
Usama K. Zafar <usamakzafar@gmail.com>
2023-02-02 14:57:19 +0100

    Update pr-description-check.ts

--- .github/workflows/scripts/pr-description-check.ts
+++ .github/workflows/scripts/pr-description-check.ts
@@ -1,6 +1,6 @@
 module.exports = async ({ github, context, core }) => {
 
-	const COMMENT_HEADER = "**No image found.**"
+	const COMMENT_HEADER = "**No image found.** ⛔️"
 	const COMMENT_BODY = "Image not found in the PR description. Please add an image to the PR description for every new key that you are adding. If you do not have an image, please request admin specifically."
 	const COMMENT_FOOTER = "If this PR is not introducing any new keys, you may ignore this message."
 	const body = `${COMMENT_HEADER} \n${COMMENT_BODY} \n${COMMENT_FOOTER}`
@@ -10,38 +10,30 @@ module.exports = async ({ github, context, core }) => {
 	var hasImage = imageRegex.test(prBody);
 
 
-    // const { data: comments } = await github.rest.issues.listComments({
-    //     owner: context.repo.owner,
-    //     repo: context.repo.repo,
-    //     issue_number: context.issue.number,
-    // })
-	    // const currentComment = comments.find(comment => comment.body.startsWith(COMMENT_HEADER))
+    const { data: comments } = await github.rest.issues.listComments({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        issue_number: context.issue.number,
+    })
+    const currentComment = comments.find(comment => comment.body.startsWith(COMMENT_HEADER))
     core.notice("Startnew Inshallah 2");
 	if (hasImage) {
 		core.notice("Image found in the PR description with updated import.");
 
 		// If PR Description has image, then remove comment (if exists)
-		// if (currentComment) {
-	    //     github.rest.issues.deleteComment({
-	    //         issue_number: context.issue.number,
-	    //         owner: context.repo.owner,
-	    //         repo: context.repo.repo,
-	    //         comment_id: currentComment.id,
-	    //     })
-	    // }
+		if (currentComment) {
+			github.rest.issues.deleteComment({
+			    issue_number: context.issue.number,
+			    owner: context.repo.owner,
+			    repo: context.repo.repo,
+			    comment_id: currentComment.id,
+			})
+	    	}
 	} else {
 	
 		core.notice("Image not found in the PR description with updated import.");
 	    // If already commented, update it
-        // if (currentComment) {
-        //     github.rest.issues.updateComment({
-        //         issue_number: context.issue.number,
-        //         owner: context.repo.owner,
-        //         repo: context.repo.repo,
-        //         comment_id: currentComment.id,
-        //         body: body
-        //     })
-        // } else {
+        if (!currentComment) {
         	// Create new comment
             github.rest.issues.createComment({
                 issue_number: context.issue.number,
@@ -49,7 +41,7 @@ module.exports = async ({ github, context, core }) => {
                 repo: context.repo.repo,
                 body: body
             })
-        // }
+        }
 		core.setFailed("Image not found in the PR description. Please add an image to the PR description for every new key that you are adding. If you do not have an image, please request admin specifically.");
 	    }
 }
